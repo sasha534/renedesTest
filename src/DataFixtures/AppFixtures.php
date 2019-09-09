39b16f956fd05ae220b96715d37245db7764c530
Олександр <s.sorokin@smida.gov.ua>
2019-09-09 13:11:51 +0300

    add AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Article;
+use DateTime;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +13,14 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $product = new Article();
+            $product->setTitle('Titile '.$i);
+            $product->setContent('Content' .$i);
+            $time = \DateTime::createFromFormat('yy-mm-dd hh:mm:ss');
+            $product->setDateCreate($time);
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
